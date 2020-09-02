938a9c70ad66ea70f63f798c7e8455098a84cafd
Mei <m_fishoo@hotmail.com>
2020-09-02 23:55:27 +0800

    room model

--- app/src/main/java/com/laioffer/tinnews/ui/home/HomeViewModel.java
+++ app/src/main/java/com/laioffer/tinnews/ui/home/HomeViewModel.java
@@ -5,12 +5,14 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Transformations;
 import androidx.lifecycle.ViewModel;
 
+import com.laioffer.tinnews.model.Article;
 import com.laioffer.tinnews.model.NewsResponse;
 import com.laioffer.tinnews.repository.NewsRepository;
 
 public class HomeViewModel extends ViewModel {
     private final NewsRepository repository;
     private final MutableLiveData<String> countryInput = new MutableLiveData<>();
+    private final MutableLiveData<Article> favoriteArticleInput = new MutableLiveData<>();
 
     public HomeViewModel(NewsRepository newsRepository) {
         this.repository = newsRepository;
@@ -20,7 +22,19 @@ public class HomeViewModel extends ViewModel {
         countryInput.setValue(country);
     }
 
+    public void setFavoriteArticleInput(Article article){
+        favoriteArticleInput.setValue(article);
+    }
+
     public LiveData<NewsResponse> getTopHeadlines(){
         return Transformations.switchMap(countryInput, repository::getTopHeadlines);
+    }
+
+    public LiveData<Boolean> onFavorite(){
+        return Transformations.switchMap(favoriteArticleInput, repository::favoriteArticle);
+    }
+
+    public void onCancel(){
+        repository.onCancel();
     }
 }
